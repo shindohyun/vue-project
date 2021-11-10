4fd1cedb8b9baa4bcc5623c971df5eeb662cd195
dohyun <shindohyunx@gmail.com>
2021-11-11 00:45:14 +0900

    Revert "axios install, mixins.js 파일 추가 및 등록"
    This reverts commit 0451c448037a88fc21414d8bee95205186edc849.

--- src/main.js
+++ src/main.js
@@ -2,10 +2,5 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import mixins from './mixins'
 
-const app = createApp(App)
-app.use(store)
-app.use(router)
-app.mixin(mixins)
-app.mount('#app')
+createApp(App).use(store).use(router).mount('#app')
